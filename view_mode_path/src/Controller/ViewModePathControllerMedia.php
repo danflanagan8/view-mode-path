9d1ea7207f940a9814222ddd2fb2ea308412acfe
Dan Flanagan <dan.flanagan@manifest.com>
2019-03-23 19:51:42 -0500

    Fix a permissions problem where unpublished content could be seen.

--- view_mode_path/src/Controller/ViewModePathControllerMedia.php
+++ view_mode_path/src/Controller/ViewModePathControllerMedia.php
@@ -4,6 +4,8 @@ namespace Drupal\view_mode_path\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\media\Entity\Media;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Class ViewModePathController
@@ -13,10 +15,15 @@ class ViewModePathControllerMedia extends ControllerBase {
 
   public function build($id = NULL, $view_mode = 'default'){
     if ($id && $media = Media::load($id)) {
-      return \Drupal::entityTypeManager()->getViewBuilder('media')->view($media, $view_mode);
+      if ($media->access('view', $this->currentUser())) {
+        return \Drupal::entityTypeManager()->getViewBuilder('media')->view($media, $view_mode);
+      }
+      else {
+        throw new AccessDeniedHttpException();
+      }
     }
     else {
-      return NULL;
+      throw new NotFoundHttpException();
     }
   }
 
--- view_mode_path/src/Controller/ViewModePathControllerNode.php
+++ view_mode_path/src/Controller/ViewModePathControllerNode.php
@@ -4,6 +4,8 @@ namespace Drupal\view_mode_path\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\Entity\Node;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Class ViewModePathController
@@ -13,10 +15,15 @@ class ViewModePathControllerNode extends ControllerBase {
 
   public function build($id = NULL, $view_mode = 'default'){
     if ($id && $node = Node::load($id)) {
-      return \Drupal::entityTypeManager()->getViewBuilder('node')->view($node, $view_mode);
+      if ($node->access('view', $this->currentUser())) {
+        return \Drupal::entityTypeManager()->getViewBuilder('node')->view($node, $view_mode);
+      }
+      else {
+        throw new AccessDeniedHttpException();
+      }
     }
     else {
-      return NULL;
+      throw new NotFoundHttpException();
     }
   }
 
